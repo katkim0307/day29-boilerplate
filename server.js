d874dbae0a03d380a641c5e9c7f807038a549b2c
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-20 15:12:36 -0400

    CREATED npm create-react-app client

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 const morgan = require('morgan');
 const port = 4000;
 
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/api/students', (req, res) => {
+    const students = [
+        {id: 1, firstName: 'Kat', lastName: 'Kim'},
+        {id: 2, firstName: 'Sue', lastName: 'Ham'},
+    ]
+    res.json(students);
+});
+
 app.listen(port, () => {
     console.log(`Server Started on Part ${port}`);
-})
+});
